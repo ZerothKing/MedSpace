7b43594196b680900f6b92986a72ae1c4c2d6ea6
akash803 <meenaakash1110@gmail.com>
2021-10-31 18:56:18 +0530

    update css

---
+++ frontend/src/component/layout/About/About.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./aboutSection.css";
+import { Button, Typography, Avatar } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import InstagramIcon from "@material-ui/icons/Instagram";
+const About = () => {
+  const visitInstagram = () => {
+    window.location = "https://instagram.com/meabhisingh";
+  };
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+
+        <div>
+          <div>
+            <Avatar
+              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+              // src="https://res.cloudinary.com/tripleayt/image/upload/v1631555947/products/jpyibarlaxawvcvqjv5b.png"
+              alt="Founder"
+            />
+            <h1>Medspace</h1>
+            {/* <Button onClick={visitInstagram} color="primary">
+              Visit Instagram
+            </Button> */}
+            <span>
+            The objective of this project is to create an online pharmacy where consumers can purchase medicines and medical instruments. E-commerce websites like Amazon and Flipkart allow us to buy everyday goods and have them delivered directly to our doorstep, but websites that do the same for medicine are not as large or efficient. This mode of purchase was especially helpful during the pandemic when buying goods in person was simply not safe. This project aims to create a convenient platform so that buying medicines is more accessible. 
+            </span>
+          </div>
+          {/* <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Brands</Typography>
+            <a
+              href="https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw"
+              target="blank"
+            >
+              <YouTubeIcon className="youtubeSvgIcon" />
+            </a>
+
+            <a href="https://instagram.com/meabhisingh" target="blank">
+              <InstagramIcon className="instagramSvgIcon" />
+            </a>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
